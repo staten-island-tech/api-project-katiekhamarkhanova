be04f71aa8d043cdb6a41516fafa58a49afaec4c
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2023-12-08 09:26:10 -0500

    api

--- api-project/main.js
+++ api-project/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-const URL ='http://dataservice.accuweather.com/currentconditions/v1/329260?apikey=BTM7UO03eyvKCMa6t4LeborVIvQK2lyT&language=en-us&details=false';
+const URL ='http://dataservice.accuweather.com/currentconditions/v1/14-349727_1_AL?apikey=%09BTM7UO03eyvKCMa6t4LeborVIvQK2lyT&language=en-us&details=true';
 
 async function getData(URL) {
     try {
@@ -12,7 +12,8 @@ async function getData(URL) {
         console.log(response);
         const data = await response.json();
         console.log(data);
-        document.querySelector("h1").textContent = data.temperature;
+/*         data.results.forEach((city)=> console.log(city)); */
+        /* document.querySelector("h1").textContent = data.temperature; */
     } catch (error) {
         console.log("sorry");
     }
