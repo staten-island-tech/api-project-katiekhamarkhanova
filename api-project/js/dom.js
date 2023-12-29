5457216ca64f3c2018f286227d3047515fce32ac
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2023-12-29 01:39:05 -0500

    weatherrrr

--- api-project/js/dom.js
+++ api-project/js/dom.js
@@ -3,4 +3,52 @@ const DOMSelectors = {
   box: document.querySelector(".container-box"),
   input: document.querySelector("#input"),
   citySearch: document.querySelector(".city-search"),
+  app: document.querySelector("#app"),
 }
+
+const userInput = DOMSelectors.input.value;
+export { userInput };
+
+DOMSelectors.form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  console.log(userInput); 
+});
+
+
+/* DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault(); */
+function addCard(data, i) {
+  function removeTime(date) {
+    let x = Array.from(date);
+    x.splice(-10)
+    const y = x.join('');
+    console.log(y);
+    return y;
+  }
+  function removeDate(time) {
+    let z = Array.from(time);
+    z.splice(0,11);
+    z.splice(5,4);
+    const w = z.join('');
+    console.log(w);
+    return w;
+  }
+  DOMSelectors.app.insertAdjacentHTML(
+    "beforeend",
+    `<div class="card">
+      <h2>${removeTime(data.timelines.daily[i].time)}</h2>
+      <h3>Temperature: ${data.timelines.daily[i].values.temperatureAvg}°F</h3>
+      <p>Cloud Cover: ${data.timelines.daily[i].values.cloudCoverAvg}%</p>
+      <p>Dew Point: ${data.timelines.daily[i].values.dewPointAvg}°F</p>
+      <p>Humidity: ${data.timelines.daily[i].values.humidityAvg}%</p>
+      <p>Precipitation Probability: ${data.timelines.daily[i].values.precipitationProbabilityAvg}%</p>
+      <p>UV Index: ${data.timelines.daily[i].values.uvIndexAvg}</p>
+      <p>Visibility: ${data.timelines.daily[i].values.visibilityAvg} miles</p>
+      <p>Wind Gust: ${data.timelines.daily[i].values.windGustAvg} mph</p>
+      <p>Sunrise: ${removeDate(data.timelines.daily[i].values.sunriseTime)} AM</p>
+      <p>Sunset: ${removeDate(data.timelines.daily[i].values.sunsetTime)} PM</p>
+    </div>`
+  );
+}
+export { addCard };
+
--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,22 +1,36 @@
 import '/css/style.css'
 import '/css/variables.css'
+import './dom.js';
+import { userInput } from "./dom.js";
+import { addCard } from "./dom.js";
 
-const URL ='https://api.tomorrow.io/v4/weather/forecast?location=London&timesteps=1d&units=metric&apikey=lrztIeax2sMJHKdeFeL2ej9MZFLJFRHx';
+
+const URL =`https://api.tomorrow.io/v4/weather/forecast?location=Staten Island&timesteps=1d&units=imperial&apikey=lrztIeax2sMJHKdeFeL2ej9MZFLJFRHx`;
 
 async function getData(URL) {
     try {
         const response = await fetch(URL);
         if (response.status !=200){
-            let pokemon = "peekachew";
-            throw new Error(pokemon);
+            throw new Error(response.statusText)
         }
         console.log(response);
         const data = await response.json();
         console.log(data);
 /*         data.results.forEach((city)=> console.log(city)); */
-        /* document.querySelector("h1").textContent = data.temperature; */
+        weatherDisplay(data);
+        
     } catch (error) {
         console.log("sorry");
     }
 }
 getData(URL);
+
+function weatherDisplay(data) {
+    document.getElementById("app2").textContent = data.location.name;
+    for (let i = 0; i < 5; i++) {
+      addCard(data, i);
+    }
+  }
+
+
+  
